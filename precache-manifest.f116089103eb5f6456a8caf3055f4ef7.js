3e4212b673d6cbcbb4bcd6364bb86b945d126b63
George Moon <george@moon.org.nz>
2019-06-29 00:48:20 +1200

    Updates

---
+++ precache-manifest.f116089103eb5f6456a8caf3055f4ef7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ccc2e09769951a916945eac51f931aa",
+    "url": "/starnaud/index.html"
+  },
+  {
+    "revision": "b05044d0d556eea38999",
+    "url": "/starnaud/static/css/main.6daaf882.chunk.css"
+  },
+  {
+    "revision": "7b811650cd2cd881c5c0",
+    "url": "/starnaud/static/js/2.5415d3b6.chunk.js"
+  },
+  {
+    "revision": "b05044d0d556eea38999",
+    "url": "/starnaud/static/js/main.7d67ce6e.chunk.js"
+  },
+  {
+    "revision": "f226d6daa6f352dab520",
+    "url": "/starnaud/static/js/runtime~main.9bd44f9e.js"
+  }
+]);
