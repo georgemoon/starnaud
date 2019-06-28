6b27bf3e50b4c1aad4d741f116b1e36848d2405d
George Moon <george@moon.org.nz>
2019-06-28 21:58:41 +1200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className="container">
+          <h1>St. Arnaud</h1>
+        </div>
+      </header>
+      <main>
+        <div className="container">
+          <p>St Arnaud weather & webcams.</p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
